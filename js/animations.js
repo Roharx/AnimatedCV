4736b231988b4eb2bf0930e5365c439f4494e2fc
Bálint <47783658+Roharx@users.noreply.github.com>
2023-02-16 01:35:25 +0100

    Added HTML content, base CSS and JS

---
+++ js/animations.js
@@ -0,0 +1,16 @@
+let mybutton = document.getElementById("back-to-top");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTo({top: 0, behavior: 'smooth'}); // For Safari
+  document.documentElement.scrollTo({top: 0, behavior: 'smooth'}); // For Chrome, Firefox, IE and Opera
+}
